e8b1ec9e928e0a9934dafa9475208dab60422ff6
Aywin <aywink@hotmail.co.uk>
2023-11-25 13:54:46 +0000

    .

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import "./Rating.css";
 
 const Rating = () => {
-  const [rating, setRating] = useState(2);
+  const [rating, setRating] = useState(5);
+  const emptyArr = [0, 0, 0, 0, 0];
 
   const handleClick = (e) => {
     console.log(e.target.parentNode.classList[0], e.target.id);
-
-    if (e.target.parentNode.classList[0] === "rating-gray") {
-      setRating(() => Number(e.target.id));
-    } else if (e.target.parentNode.classList[0] === "rating") {
-    //   setRating((prev) => Number(e.target.id) - prev + 1);
-    }
+    setRating(() => Number(e.target.id));
   };
 
   return (
@@ -30,25 +26,13 @@ const Rating = () => {
           Rating: <span>{rating}</span>
         </p>
         <div className="rating-container">
-          <span className="rating-gray">
-            <span id="1">⭐</span>
-            <span id="2">⭐</span>
-            <span id="3">⭐</span>
-            <span id="4">⭐</span>
-            <span id="5">⭐</span>
-          </span>
-          <span
-            style={{
-              translate: `-${((5 - rating) / 5) * 13 * 0.98}ch 0px`,
-            }}
-            className="rating"
-          >
-            <span id="1">⭐</span>
-            <span id="2">⭐</span>
-            <span id="3">⭐</span>
-            <span id="4">⭐</span>
-            <span id="5">⭐</span>
-          </span>
+          {emptyArr.map((el, i) => (
+            <span
+              id={i + 1}
+              key={i}
+              className={`star ${i < rating ? "rating" : "rating-gray"}`}
+            ></span>
+          ))}
         </div>
       </div>
     </>
