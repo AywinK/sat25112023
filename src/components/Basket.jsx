53e83be53931305bd0a6e20891ea1bd6656935fb
Aywin <AywinK@hotmail.com>
2023-12-05 01:37:42 +0000

    adds subtotal and total amount with regex formatting to basket page

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -11,6 +11,27 @@ const Basket = ({ basket, dispatch }) => {
           dispatch={dispatch}
         />
       )).reverse()}
+
+      <hr
+        style={{
+          marginTop: "5px",
+          border: "3px ridge #787878",
+          borderRadius: "20px"
+        }}
+      />
+
+      <p style={{
+        textAlign: "end",
+        fontSize: "0.6em",
+        fontStyle: "italic"
+      }}>Subtotal: £{basket.reduce((total, product) => total + product.quantity * product.price, 0).toFixed(2).split(/(?<=\d)(?=(?:\d{3})+(?:\.\d+)?$)/).join(",")}</p>
+      <p style={{
+        textAlign: "end",
+        fontSize: "0.65em",
+        fontWeight: "500"
+      }}
+
+      >Total (inc. VAT): £{(basket.reduce((total, product) => total + product.quantity * product.price, 0) * 1.2).toFixed(2).split(/(?<=\d)(?=(?:\d{3})+(?:\.\d+)?$)/).join(",")}</p>
     </div>
   );
 };
